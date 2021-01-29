d570f9de789ff32a98b509c7b469a526eb89cfd4
Kevin Eliezer <kevineli.gp@gmail.com>
2021-01-29 21:58:07 +0000

    Merge fixes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <footer class="text-white bg-jti-black h-32">
     <div
       class="wrapper h-full flex justify-center flex-col-reverse md:flex-row"
@@ -18,16 +17,6 @@
           <font-awesome-icon class="self-center" :icon="['fab', 'twitter']" />
         </div>
         <span class="font-bold self-center mt-2">jtiesit@ull.edu.es</span>
-=======
-  <footer class="text-white bg-jti-black h-64">
-    <div class="wrapper flex">
-      <div class="w-2/4 h-full">
-        Bloque de Contacto:
-        Logo, fechas, correo, lugar, redes sociales
-      </div>
-      <div class="w-2/4 h-full">
-        Bloque de otras páginas:
->>>>>>> 954403950531e734dd1bf28d907c5e9c00c3e9d4
       </div>
     </div>
   </footer>
