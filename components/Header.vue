dd4ac0c2be535444691f068edc7e5150337f2087
Kevin Eliezer <kevineli.gp@gmail.com>
2021-02-01 09:24:48 +0000

    Merge fixes

--- components/Header.vue
+++ components/Header.vue
@@ -2,19 +2,11 @@
   <nav
     class="z-10 flex items-center p-3 flex-wrap bg-gradient-to-r from-jti-black to-jti-black sticky top-0 w-full"
   >
-<<<<<<< HEAD
     <NuxtLink to="/2021/">
       <div class="mx-6 md:mx-10 inline-flex items-center">
         <img
           class="bg-contain h-20 w-32"
           src="../assets/LogoJTI_negativo.svg"
-=======
-    <NuxtLink to="/2021/programa">
-      <div class="mx-6 md:mx-10 inline-flex items-center">
-        <img
-          class="bg-contain h-20 w-32"
-          src="../assets/LogoJTI_negativo2.svg"
->>>>>>> b305212b8ff1f73e9ad6a0f84d32581d7d2ddcd5
           alt="Jornada Técnica de Ingeniería"
         >
         <!--  <span class="text-jti-white h-12 w-40 ml-6 font-bold">Jornadas Técnica de Ingeniería</span>-->
