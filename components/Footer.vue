ba39b4eb7775113f8bd54536342c6604b77e8bed
Kevin Eliezer García Peña <71667433+kevinEliezer@users.noreply.github.com>
2021-02-11 09:04:50 +0000

    Fixed some issues and improved design (#7)
    * Page changes
    
    * Component fixes

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,18 +3,30 @@
     <div
       class="wrapper h-full flex justify-center flex-col-reverse lg:flex-row"
     >
-      <div class="lg:w-1/2 font-bold flex text-xs md:text-base pb-2 justify-center">
-        <p class="self-center">
+      <div
+        class="lg:w-1/2 font-bold flex text-xs md:text-base pb-2 justify-center"
+      >
+        <p class="text-xs md:text-base self-center">
           ©2021 Jornadas Técnicas de Ingeniería. Todos los derechos reservados
         </p>
       </div>
-      <div
-        class="lg:w-1/2 h-full flex flex-col justify-center"
-      >
+      <div class="lg:w-1/2 h-full flex flex-col justify-center">
         <div class="space-x-8 text-3xl flex self-center">
-          <font-awesome-icon class="self-center" :icon="['fab', 'linkedin']" />
-          <font-awesome-icon class="self-center" :icon="['fab', 'instagram']" />
-          <font-awesome-icon class="self-center" :icon="['fab', 'twitter']" />
+          <a href="">
+            <font-awesome-icon
+              class="self-center"
+              :icon="['fab', 'linkedin']"
+            />
+          </a>
+          <a href="https://www.instagram.com/jtiesit/">
+            <font-awesome-icon
+              class="self-center"
+              :icon="['fab', 'instagram']"
+            />
+          </a>
+          <a href="https://twitter.com/jtiesit">
+            <font-awesome-icon class="self-center" :icon="['fab', 'twitter']" />
+          </a>
         </div>
         <span class="font-bold self-center mt-2">jtiesit@ull.edu.es</span>
       </div>
--- components/InformationBlock.vue
+++ components/InformationBlock.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="w-full h-64 bg-gradient-to-tr from-jti-blue to-jti-purple-750">
+  <div class="h-48 w-full sm:h-64 bg-gradient-to-tr from-jti-blue to-jti-purple-750">
     <div
-      class="wrapper w-full flex flex-col h-full justify-center content-center flex-wrap lg:content-start"
+      class="text-sm md:text-base wrapper w-full flex flex-col h-full justify-center content-center flex-wrap lg:content-start"
     >
       <h1>{{ title }}</h1>
-      <h3 class="self-center text-xl md:text-3xl lg:self-start">{{ description }}</h3>
+      <h3 class="text-base self-center md:text-3xl lg:self-start">{{ description }}</h3>
     </div>
   </div>
 </template>
