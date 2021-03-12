d378d5f6395c871279f44696cdc852ed8ff9138a
Kevin Eliezer <kevineli.gp@gmail.com>
2021-03-12 19:16:15 +0000

    Google header

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'google-site-verification', content: 'xDusxuEEolG26id-rGfhODsneJbHcTyCOsQw_bnG6hY' }
     ]
   },
 
