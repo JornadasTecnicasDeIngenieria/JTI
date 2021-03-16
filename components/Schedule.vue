37847e8470cdc1885b50c758a909127e99389786
Kevin Eliezer <kevineli.gp@gmail.com>
2021-03-16 14:40:19 +0000

    Fix schedule

--- components/Schedule.vue
+++ components/Schedule.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="wrapper pt-6">
-      <h2 class="text-center lg:text-left">Salas Meet</h2>
+      <h2 class="text-center lg:text-left">
+        Salas Meet
+      </h2>
 
       <div class="py-3 px-4 md:px-0">
         <div>
@@ -23,12 +25,14 @@
     </div>
 
     <div class="wrapper py-6">
-      <h2 class="text-center lg:text-left">Miércoles 17 de marzo</h2>
+      <h2 class="text-center lg:text-left">
+        Miércoles 17 de marzo
+      </h2>
       <div class="shadow overflow-x-scroll md:overflow-hidden rounded border-b border-gray-200 my-8">
         <table class="min-w-full bg-white text-center">
           <thead class="bg-jti-blue text-white uppercase">
             <tr>
-              <th class="w-40"></th>
+              <th class="w-40" />
               <th class="py-3 px-4 w-60">
                 <a href="https://meet.google.com/hfi-dsrs-wws" target="blank_">Sala 1: GII</a>
               </th>
@@ -45,53 +49,87 @@
           </thead>
           <tbody class="text-gray-700">
             <tr>
-              <td class="py-3 px-4 font-bold">15:00 - 16:00</td>
-              <td class="py-3 px-4"><b>Prácticas Externas</b></br>Carmen Elvira Ramos Domínguez</br>(Subdirectora Relaciones Institucionales
-Sección Informática)
-</td>
-              <td class="py-3 px-4" colspan="3"><b>Prácticas Externas</b></br>Vicente José Romero Ternero</br> (Subdirector Relaciones Institucionales
-Sección Industrial)
-</td>
+              <td class="py-3 px-4 font-bold">
+                15:00 - 16:00
+              </td>
+              <td class="py-3 px-4">
+                <b>Prácticas Externas</b></br>Carmen Elvira Ramos Domínguez</br>(Sección informática)
+              </td>
+              <td class="py-3 px-4" colspan="3">
+                <b>Prácticas Externas</b></br>Vicente José Romero Ternero</br> (Sección Industrial)
+              </td>
             </tr>
             <tr class="bg-gray-100">
-              <td class="py-3 px-4 font-bold">16:00 - 17:00</td>
-              <td class="py-3 px-4"><b>Seguridad y verificabilidad en voto electrónico</b></br>Jordi Cucurull <br>(Scytl)</td>
-              <td class="py-3 px-4"><b>Compañía Cervecera de Canarias</b></br>Iván Lugo Hernández</br>(CCC)</td>
-              <td class="py-3 px-4"><b>Tu creatividad es la innovación</b></br>Javier Lamas</br>(Trend Robotics)</td>
-              <td class="py-3 px-4"><b>TEIDESAT, primer nanosatélite de estudiantes de universidades canarias</b></br>Joshua Barrios Pérez</br>(Egresado ULL - IAC)</td>
+              <td class="py-3 px-4 font-bold">
+                16:00 - 17:00
+              </td>
+              <td class="py-3 px-4">
+                <b>Seguridad y verificabilidad en voto electrónico</b></br>Jordi Cucurull <br>(Scytl)
+              </td>
+              <td class="py-3 px-4">
+                <b>Compañía Cervecera de Canarias</b></br>Iván Lugo Hernández</br>(CCC)
+              </td>
+              <td class="py-3 px-4">
+                <b>Tu creatividad es la innovación</b></br>Javier Lamas</br>(Trend Robotics)
+              </td>
+              <td class="py-3 px-4">
+                <b>TEIDESAT, primer nanosatélite de estudiantes de universidades canarias</b></br>Joshua Barrios Pérez</br>(Egresado ULL - IAC)
+              </td>
             </tr>
 
             <tr>
-              <td class="py-3 px-4 font-bold">17:00 - 17:30</td>
-              <td class="text-center font-bold" colspan="4">DESCANSO</td>
+              <td class="py-3 px-4 font-bold">
+                17:00 - 17:30
+              </td>
+              <td class="text-center font-bold" colspan="4">
+                DESCANSO
+              </td>
             </tr>
 
             <tr class="bg-gray-100">
-              <td class="py-3 px-4 font-bold">17:30 - 18:30</td>
-              <td class="py-3 px-4"><b>Bioinformática para informáticos. Nuestra aportación para la nueva era.</b></br>Héctor Rodríguez Pérez</br>(Egresado ULL)</td>
-              <td class="py-3 px-4"><b>Schreiber de Canarias</b></br>Riquelme Elías</br>(Egresado ULL)</td>
-              <td class="py-3 px-4"><b>Conociendo a Red Eléctrica Corporación</b></br>Juan Fernando Figueras Torres</br>(Profesor ULL)</td>
-              <td class="py-3 px-4"><b>¿Qué te espera tras el fin de la carrera?</b></br>Cintia Pérez Gutiérrez</br>(Egresada ULL - HP)</td>
+              <td class="py-3 px-4 font-bold">
+                17:30 - 18:30
+              </td>
+              <td class="py-3 px-4">
+                <b>Bioinformática para informáticos. Nuestra aportación para la nueva era.</b></br>Héctor Rodríguez Pérez</br>(Egresado ULL)
+              </td>
+              <td class="py-3 px-4">
+                <b>Schreiber de Canarias</b></br>Riquelme Elías</br>(Egresado ULL)
+              </td>
+              <td class="py-3 px-4">
+                <b>Conociendo a Red Eléctrica Corporación</b></br>Juan Fernando Figueras Torres</br>(Profesor ULL)
+              </td>
+              <td class="py-3 px-4">
+                <b>¿Qué te espera tras el fin de la carrera?</b></br>Cintia Pérez Gutiérrez</br>(Egresada ULL - HP)
+              </td>
             </tr>
 
             <tr>
-              <td class="py-3 px-4 font-bold">18:30 - 19:30</td>
-              <td class="py-3 px-4"><b>Itinerarios de Informática:</b> experiencia del alumnado</br></br><b>Alternativa: </b> Charla DISA</td>
-              <td class="py-3 px-4"><b>Conoce al Grupo DISA</b></br>Alberto Sabaté
-</br>Tinerfe Martín
-</br>Carlos Jara
-</br>Luis Medina Moreno
-</br>Jonathan Clemente
-</br>Sergio Díaz
-</br>Rebeca Alcaide
-</td>
-              <td class="py-3 px-4"><b>Mesa redonda: Cómo enfrentarse a un TFG/TFM</b></br>Luisana Lara Briceño
-</br>(Estudiante ULL)
-</br>Rita Ramírez Akhmoldaeva </br>(Estudiante ULL)
-</br>Humberto José González Olivera
-</br>(Egresado ULL)
-</td>
-              <td class="py-3 px-4"><b>Ingenieros en diseño, ¿qué somos?</b></br>Alejandro Martí Gil</br>(Estudiante ULPGC)</td>
+              <td class="py-3 px-4 font-bold">
+                18:30 - 19:30
+              </td>
+              <td class="py-3 px-4">
+                <b>Itinerarios de Informática:</b> experiencia del alumnado</br></br><b>Alternativa: </b> Charla DISA
+              </td>
+              <td class="py-3 px-4">
+                <b>Conoce al Grupo DISA</b></br>Alberto Sabaté
+                </br>Tinerfe Martín
+                </br>Carlos Jara
+                </br>Luis Medina Moreno
+                </br>Jonathan Clemente
+                </br>Sergio Díaz
+                </br>Rebeca Alcaide
+              </td>
+              <td class="py-3 px-4">
+                <b>Mesa redonda: Cómo enfrentarse a un TFG/TFM</b></br>Luisana Lara Briceño
+                </br>(Estudiante ULL)
+                </br>Rita Ramírez Akhmoldaeva </br>(Estudiante ULL)
+                </br>Humberto José González Olivera
+                </br>(Egresado ULL)
+              </td>
+              <td class="py-3 px-4">
+                <b>Ingenieros en diseño, ¿qué somos?</b></br>Alejandro Martí Gil</br>(Estudiante ULPGC)
+              </td>
             </tr>
           </tbody>
         </table>
@@ -99,12 +137,14 @@ Sección Industrial)
     </div>
 
     <div class="wrapper py-6">
-      <h2 class="text-center lg:text-left">Jueves 18 de marzo</h2>
+      <h2 class="text-center lg:text-left">
+        Jueves 18 de marzo
+      </h2>
       <div class="shadow overflow-x-scroll md:overflow-hidden rounded border-b border-gray-200 my-8">
         <table class="min-w-full bg-white text-center">
           <thead class="bg-jti-blue text-white uppercase">
             <tr>
-              <th class="w-40"></th>
+              <th class="w-40" />
               <th class="py-3 px-4 w-60">
                 <a href="https://meet.google.com/hfi-dsrs-wws" target="blank_">Sala 1: GII</a>
               </th>
@@ -121,62 +161,98 @@ Sección Industrial)
           </thead>
           <tbody class="text-gray-700">
             <tr>
-              <td class="py-3 px-4 font-bold">15:00 - 16:00</td>
-              <td class="py-3 px-4"><b>COITIC</b></br>Juan Crespo Naón (Decano)</td>
-              <td class="py-3 px-4" colspan="3"><b>COITITF y MUPITI</b></br>Antonio M. Rodríguez Hernández</br>(Decano y Vicepresidente)</td>
+              <td class="py-3 px-4 font-bold">
+                15:00 - 16:00
+              </td>
+              <td class="py-3 px-4">
+                <b>COITIC</b></br>Juan Crespo Naón <br>(Decano)
+              </td>
+              <td class="py-3 px-4" colspan="3">
+                <b>COITITF y MUPITI</b></br>Antonio M. Rodríguez Hernández</br>(Decano y Vicepresidente)
+              </td>
             </tr>
             <tr class="bg-gray-100">
-              <td class="py-3 px-4 font-bold">16:00 - 17:00</td>
-              <td class="py-3 px-4"><b>Cómo cambiar el mundo con lo que he aprendido en la carrera</b></br>Alejandro González</br>(Acid Tango)</td>
-              <td class="py-3 px-4"><b>La ingeniería de proyectos. El ingeniero y la empresa de ingeniería</b><br>Juan Domingo Acosta García<br>IGS Ingenieros</td>
-              <td class="py-3 px-4"><b>¿Quiénes somos y qué buscamos?</b><br>Fátima Fuertes Neira<br>
-(Ferrovial)
-</td>
-              <td class="py-3 px-4"><b>Ingeniería y mantenimiento</b><br>Sara Estévez Pérez (Metrotenerife)</td>
+              <td class="py-3 px-4 font-bold">
+                16:00 - 17:00
+              </td>
+              <td class="py-3 px-4">
+                <b>Cómo cambiar el mundo con lo que he aprendido en la carrera</b></br>Alejandro González</br>(Acid Tango)
+              </td>
+              <td class="py-3 px-4">
+                <b>La ingeniería de proyectos. El ingeniero y la empresa de ingeniería</b><br>Juan Domingo Acosta García<br>(IGS Ingenieros)
+              </td>
+              <td class="py-3 px-4">
+                <b>¿Quiénes somos y qué buscamos?</b><br>Fátima Fuertes Neira<br>
+                (Ferrovial)
+              </td>
+              <td class="py-3 px-4">
+                <b>Ingeniería y mantenimiento</b><br>Sara Estévez Pérez (Metrotenerife)
+              </td>
             </tr>
 
             <tr>
-              <td class="py-3 px-4 font-bold">17:00 - 17:30</td>
-              <td class="text-center font-bold" colspan="4">DESCANSO</td>
+              <td class="py-3 px-4 font-bold">
+                17:00 - 17:30
+              </td>
+              <td class="text-center font-bold" colspan="4">
+                DESCANSO
+              </td>
             </tr>
 
             <tr class="bg-gray-100">
-              <td class="py-3 px-4 font-bold">17:30 - 18:30</td>
-              <td class="py-3 px-4"><b>Trabajar fuera, la otra alternativa</b></br>Raúl Marrero Rodríguez<br>(Egresado ULL - Riot Games)</td>
-              <td class="py-3 px-4"><b>Frimancha Canarias</b><br>Christian Martín González</td>
-              <td class="py-3 px-4"><b>El Área de Instrumentación del Instituto de Astrofísica de Canarias</b></br>Jose Alfonso López Aguerri<br>(IAC)</td>
-              <td class="py-3 px-4"><b>Optimización Topológica</b></br>Francisco José Gala (ORIGEN CAD CAM CAE)</td>
+              <td class="py-3 px-4 font-bold">
+                17:30 - 18:30
+              </td>
+              <td class="py-3 px-4">
+                <b>Trabajar fuera, la otra alternativa</b></br>Raúl Marrero Rodríguez<br>(Egresado ULL - Riot Games)
+              </td>
+              <td class="py-3 px-4">
+                <b>Frimancha Canarias</b><br>Christian Martín González
+              </td>
+              <td class="py-3 px-4">
+                <b>El Área de Instrumentación del Instituto de Astrofísica de Canarias</b></br>Jose Alfonso López Aguerri<br>(IAC)
+              </td>
+              <td class="py-3 px-4">
+                <b>Optimización Topológica</b></br>Francisco José Gala </br>(ORIGEN CAD CAM CAE)
+              </td>
             </tr>
 
             <tr>
-              <td class="py-3 px-4 font-bold">18:30 - 19:30</td>
-              <td class="py-3 px-4" colspan="2"><b>Psicología Laboral</b><br>Eduardo Rosado Cruz
-<br>(Egresado ULL)
-</td>
-              <td class="py-3 px-4"><b>Mesa redonda en Energías Renovables</b></br>Marcial González de Armas
-<br>(Egresado ULL - Capitán Renovable)
-<br>Ricardo Luis Guerrero Lemus
-<br>(Catedrático ULL)
-<br>Elena Rodriguez Clemente
-<br>(Egresada ULL)
-</td>
-              <td class="py-3 px-4"><b>Proyectos ESIT: ULL-Dynamics</b></br>Guillermo Coba Afonso
-<br>(Estudiante ULL)
-<br>Yared Adrián Pestana Santana
-<br>(Estudiante ULL)
-</td>
+              <td class="py-3 px-4 font-bold">
+                18:30 - 19:30
+              </td>
+              <td class="py-3 px-4" colspan="2">
+                <b>Psicología Laboral</b><br>Eduardo Rosado Cruz
+                <br>(Egresado ULL)
+              </td>
+              <td class="py-3 px-4">
+                <b>Mesa redonda en Energías Renovables</b></br>Marcial González de Armas
+                <br>(Egresado ULL - Capitán Renovable)
+                <br>Ricardo Luis Guerrero Lemus
+                <br>(Catedrático ULL)
+                <br>Elena Rodriguez Clemente
+                <br>(Egresada ULL)
+              </td>
+              <td class="py-3 px-4">
+                <b>Proyectos ESIT: ULL-Dynamics</b></br>Guillermo Coba Afonso
+                <br>(Estudiante ULL)
+                <br>Yared Adrián Pestana Santana
+                <br>(Estudiante ULL)
+              </td>
             </tr>
           </tbody>
         </table>
       </div>
     </div>
-     <div class="wrapper py-4">
-      <h2 class="text-center lg:text-left">Talleres</h2>
+    <div class="wrapper py-4">
+      <h2 class="text-center lg:text-left">
+        Talleres
+      </h2>
       <div class="md:flex">
         <div class="my-4 w-100 md:w-120 p-4">
           <div><b>Título: </b> Inmersión Práctica al Mundo FIWARE (IoT)</div>
           <div>
-            <b>Descripción: </b> <br />
+            <b>Descripción: </b> <br>
             Se pretende en este taller, profundizar de una manera básica e
             introductora en el aspecto más técnico respecto a las tecnologías
             FIWARE. Se trabajará en la instalación del CONTEXT BROKER de FIWARE
@@ -191,15 +267,14 @@ Sección Industrial)
               class="text-blue-700"
               href="https://zfrmz.eu/MFuO3gq8GtI4vfFsAgyP"
               target="blank_"
-              >https://zfrmz.eu/MFuO3gq8GtI4vfFsAgyP</a
-            >
+            >https://zfrmz.eu/MFuO3gq8GtI4vfFsAgyP</a>
           </div>
           <div><b>Autor: </b> Innovalia Group</div>
         </div>
         <div class="my-4 w-100 md:w-120 p-4">
           <div><b>Título: </b> Desarrollo de videojuegos en Unreal</div>
           <div>
-            <b>Descripción: </b> <br />
+            <b>Descripción: </b> <br>
             Introducción a desarrollo de videojuegos utilizando Unreal.
           </div>
           <div><b>Fecha: </b> Viernes 19 de Marzo (16:00)</div>
@@ -210,8 +285,7 @@ Sección Industrial)
               class="text-blue-700"
               href="https://zfrmz.eu/4zPnIU1lbBgPiftz3q4m"
               target="blank_"
-              >https://zfrmz.eu/4zPnIU1lbBgPiftz3q4m</a
-            >
+            >https://zfrmz.eu/4zPnIU1lbBgPiftz3q4m</a>
           </div>
           <div><b>Autor: </b> Tenerife Juega</div>
         </div>
