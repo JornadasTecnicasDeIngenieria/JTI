7dfed8c439618452e8a7c3597fb359439e63fe60
Kevin Eliezer <kevineli.gp@gmail.com>
2021-08-29 20:19:37 +0100

    feat pantalla ediciones

---
+++ components/HeaderEditions.vue
@@ -0,0 +1,82 @@
+<template>
+  <nav
+    class="
+      z-10
+      flex
+      items-center
+      p-3
+      flex-wrap
+      bg-gradient-to-r bg-jti-purple
+      sticky
+      top-0
+      w-full
+      flex
+    "
+  >
+    <div class="mx-6 md:mx-10">
+      <NuxtLink to="/2021/">
+        <div class="inline-flex items-center">
+          <img
+            class="bg-contain h-20 w-32"
+            src="../assets/LogoJTI_negativo.svg"
+            alt="Jornada Técnica de Ingeniería"
+          />
+        </div>
+      </NuxtLink>
+    </div>
+    <button
+      class="
+        text-jti-white
+        inline-flex
+        p-3
+        rounded
+        lg:hidden
+        ml-auto
+        hover:text-white
+        outline-none
+        nav-toggler
+      "
+    >
+      <i ref="menuicon" class="material-icons">menu</i>
+    </button>
+    <div
+      ref="navigation"
+      class="hidden top-navbar w-full lg:inline-flex lg:w-auto text-jti-white"
+    >
+      <div
+        ref="menu"
+        class="
+          flex flex-wrap flex-col
+          lg:flex-row
+          lg:ml-auto
+          lg:w-auto
+          w-full
+          lg:items-center
+          lg:h-auto
+        "
+      ></div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeaderEditions",
+};
+</script>
+<style scoped>
+#navigation {
+  font-family: "Rubik", sans-serif;
+}
+
+.textunderline:hover {
+  text-decoration: underline;
+  text-decoration-color: #1d9cd8;
+  text-underline-offset: 4px;
+  text-decoration-thickness: 2px;
+}
+
+.active {
+  font-weight: bold;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="w-full header-offset flex justify-center align-center">
+    <a
+      href="https://eventos.ull.es/go/jtiesit2019"
+      class="
+        w-40
+        h-40
+        rounded-xl
+        bg-jti-purple-750
+        text-white text-4xl
+        m-12
+        hover:bg-jti-purple
+        flex
+        content-center
+        justify-center
+      "
+      target="_blank"
+    >
+      <span class="mt-10"> 2019 </span>
+    </a>
+    <NuxtLink
+      to="/2021"
+      class="
+        w-40
+        h-40
+        rounded-xl
+        bg-jti-purple-750
+        text-white text-4xl
+        m-12
+        hover:bg-jti-purple
+        flex
+        content-center
+        justify-center
+      "
+    >
+      <div class="mt-10">2021</div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "editions",
+};
+</script>
+
+<style>
+.header-offset {
+  min-height: calc(100vh - 111px);
+}
+</style>
